564c99ceb781f323f7f4bd2458006c3dbc67fa61
SoloSynth1 <orixay@hotmail.com>
2021-11-15 21:38:20 +0800

    update styling

--- src/components/molecules/Home.vue
+++ src/components/molecules/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
     <h1>Coder's Toolbox</h1>
-    <span>A trusty toolbox for all your coding needs.</span>
+    <div>A trusty toolbox for all your coding needs.</div>
+    <div><strong>Secure. Fast. No data transfers. No backends called. Works perfectly even offline.</strong></div>
     <h6>Developed by <a href="https://github.com/SoloSynth1">SoloSynth1</a>, 2021</h6>
   </div>
 </template>
@@ -12,3 +13,9 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style scoped>
+div {
+  padding: 0.5rem;
+}
+</style>
